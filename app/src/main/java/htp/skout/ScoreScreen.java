d6bfb84816790b6953f7af602ccfe35c3bae7329
ZachPurdue <perryz@purdue.edu>
2015-08-15 15:24:16 -0700

    set up basic scorescreen functionality with placeholders

--- app/src/main/java/htp/skout/ScoreScreen.java
+++ app/src/main/java/htp/skout/ScoreScreen.java
@@ -1,9 +1,14 @@
 package htp.skout;
 
-import android.support.v7.app.AppCompatActivity;
+//Zach Perry - hack the planet
+//ZachPurdue github
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class ScoreScreen extends AppCompatActivity {
 
@@ -34,4 +39,54 @@ public class ScoreScreen extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    //Ride Breakdown-------------------------------------------------------------------------------
+
+    public float getDistance() { //return distance of trip in miles (mi)
+        return 89.41; //placeholder
+    }
+
+    public int getDuration() { //return the duration of the ride in minutes (m), rounded down
+        //distance / inverse of average speed * 60 == duration
+        return 150; //placeholder
+    }
+
+    public float getAvgSpeed() { //return average speed of the trip in miles per hour (mph)
+        return 35.64 //placeholder
+    }
+
+    public float getTopSpeed() { //return top speed of trip in miles per hour (mph)
+        return 72.43; //placeholder
+    }
+
+    //Violations Breakdown-------------------------------------------------------------------------
+
+    public int getViolations() { //return amount of times the speed limit was violated
+        int n = 4;
+        if (n = 0) {
+            return 0;
+            //if no violations, show some other congratulationary message
+        }
+        else {
+            return n; //placeholder
+        }
+    }
+
+    public float getAverageViolation() { //return average speed above speed limit per violation
+        return 3.64; //placeholder
+    }
+
+    //Score----------------------------------------------------------------------------------------
+    /*
+    Uses the scoring algorithm to assign the rider a score based on distance, speed, and
+    violations during the ride.
+     */
+    /*
+    In the end, algorithm should take all collected data points into account for a score that
+    is as accurate as possible.
+     */
+    public int rideScore() { //method yet to be decided
+        return 1124; //placeholder
+    }
+
 }
