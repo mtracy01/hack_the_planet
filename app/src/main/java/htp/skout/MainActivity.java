844a92d7a0a330448293123b67e7ffd37d2315af
mtracy01 <matthetracy@gmail.com>
2015-08-15 11:42:59 -0400

    Added satellite menu libraries

--- app/src/main/java/htp/skout/MainActivity.java
+++ app/src/main/java/htp/skout/MainActivity.java
@@ -4,13 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
     }
 
     @Override
