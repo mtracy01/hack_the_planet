c437d74772635f12558b31631a00806f04d9637d
thefreshduke <scottyshaw1@gmail.com>
2015-08-16 02:03:42 +0100

    Completed network request for weather information

---
+++ app/src/main/java/htp/skout/Tuple.java
@@ -0,0 +1,14 @@
+package htp.skout;
+
+/**
+ * Created by TheFreshDuke on 8/16/15.
+ */
+public class Tuple<X, Y> {
+    public final X x;
+    public final Y y;
+
+    public Tuple(X x, Y y) {
+        this.x = x;
+        this.y = y;
+    }
+}
